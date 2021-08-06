fc6fe33522361c455fabb00182e3387425279936
Martin Chan <martinchan53@gmail.com>
2021-08-06 17:41:34 +0100

    feat: add new functions for summarising package

---
+++ R/extract_argument.R
@@ -0,0 +1,39 @@
+#' @title Extract the arguments of a function for a loaded package
+#'
+#' @description Returns a character vector containing the arguments of a
+#'   function for a loaded package. Arguments are separated by semi-colons.
+#'
+#' @param fnc_name String containing the name of the function.
+#' @param package_name String containing the name of the loaded package.
+#'
+#' @examples
+#' extract_argument(
+#'   fnc_name = "extract_argument",
+#'   package_name = "devtoolbox")
+#'
+#' @import utils
+#'
+#' @export
+
+extract_argument <- function(fnc_name, package_name){
+
+  fnc_name %>%
+    utils::help(eval(package_name)) %>%
+    get_help_file() %>% # duplicate of utils:::.getHelpFile()
+    purrr::keep(~attr(.x, "Rd_tag") == "\\arguments") %>%
+    purrr::map(as.character) %>%
+    purrr::flatten_chr() -> p
+
+  if(length(p) == 0){
+    return("")
+  } else {
+    p %>%
+      stringr::str_remove_all(., "[\n]") %>%
+      .[. != ""] %>% # remove blanks
+      stringr::str_trim() %>%
+      stringr::str_remove_all("list\\(") %>% # clean up
+      stringr::str_remove_all(",.+") %>% # remove argument description
+      stringr::str_remove_all("[:punct:]") %>% # remove all non-words
+      paste(collapse = "; ")
+  }
+}
---
+++ R/extract_title.R
@@ -0,0 +1,28 @@
+#' @title Extract the title of a function for a loaded package
+#'
+#' @description Returns a character vector containing the title of a
+#'   function for a loaded package.
+#'
+#' @param fnc_name String containing the name of the function.
+#' @param package_name String containing the name of the loaded package.
+#'
+#' @examples
+#' extract_title(
+#'   fnc_name = "extract_title",
+#'   package_name = "devtoolbox")
+#'
+#' @import utils
+#'
+#' @export
+
+extract_title <- function(fnc_name, package_name){
+  fnc_name %>%
+    utils::help(eval(package_name)) %>%
+    get_help_file() %>% # duplicate of utils:::.getHelpFile()
+    purrr::keep(~attr(.x, "Rd_tag") == "\\title") %>%
+    purrr::map(as.character) %>%
+    purrr::flatten_chr() %>%
+    paste0(., collapse="") %>%
+    stringr::str_remove("[\n]") %>%
+    stringr::str_trim()
+}
